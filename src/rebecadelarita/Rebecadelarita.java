452c0705961e3db97df9da41e7e4234b9efbe586
Dyubli <diovelyargawanon@gmai.com>
2024-09-11 17:00:10 +0800

    first commit

---
+++ src/rebecadelarita/Rebecadelarita.java
@@ -0,0 +1,37 @@
+
+package rebecadelarita;
+
+public class Rebecadelarita {
+
+  
+  class scholarshipApplication{
+      //Varliable to hold the application details
+      private int appplicationId;
+      private String name;
+      private double gpa;
+      private double annualFamilyIncome;
+     AnnualFamilyIncome;
+     private int
+     communityServiceHours;
+     // Method to input
+     application details;
+     public void enterDettails(){
+                 
+         Scanner scanner = new;
+             Scanner (system.in);
+          System.out.print("EnterApplication ID:");
+          applicationID = scanner. nextInt();
+          
+          scanner.nextLine();
+          Consume newline left-over
+               
+          
+         
+     }
+             
+     
+     
+              
+    }
+    
+}
